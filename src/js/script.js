e7501bc9030691f73c8958cfc28a89ce3808225a
ostapvasulenko <ostapvasilenko55@gmail.com>
2023-02-12 14:28:13 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,16 @@
-document.getElementById("btn_first").addEventListener('click', (e) =>{
-    e.preventDefault();
-    alert('Hello!')
-})
+const FORM = document.querySelector("form");
+const FNAME = document.getElementById("fname");
+const LNAME = document.getElementById("lname");
+const ABOUT_ME = document.getElementsByClassName("about__me");
+const LOCATION_DATA = document.getElementsByClassName("location");
 
+FORM.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const FIRST_NAME = FNAME.value;
+    const LAST_NAME = LNAME.value;
+    const ABOUT = ABOUT_ME.value;
+
+    console.log(`Fisrt Name: ${FIRST_NAME}`);
+    console.log(`Last Name: ${LAST_NAME}`);
+    console.log(`About me: ${ABOUT}`);
+})
