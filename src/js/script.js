5e004856bf8073c3565a485582e1014cb99d64cc
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-02-20 11:53:23 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -4,21 +4,34 @@ const LNAME = document.getElementById("lname");
 const ABOUT_ME = document.getElementById("about__me");
 const LOCATION_DATA = document.getElementById("location");
 const LANGUAGE_USER = document.getElementById("lang");
-const WORK_USER = document.getElementById("experience_work");
+const WORK_USER = document.querySelectorAll(`input[type="checkbox"]`);;
 
 FORM.addEventListener("submit", (event) => {
     event.preventDefault();
     const FIRST_NAME = FNAME.value;
+    function capitalize(FNAME) {
+        return `${FNAME.charAt(0).toUpperCase()}${FNAME.slice(1)}`;
+    }
+    console.log('First Name: ' + capitalize(`${FIRST_NAME}`))
+    
     const LAST_NAME = LNAME.value;
+    function capitalize(LNAME) {
+        return `${LNAME.charAt(0).toUpperCase()}${LNAME.slice(1)}`;
+    }
+    console.log('Last Name: ' + capitalize(`${LNAME}`))
     const ABOUT = ABOUT_ME.value;
     const LOCATION = LOCATION_DATA.value;
     const LANGUAGE = LANGUAGE_USER.value;
-    const WORK = WORK_USER.value;
-
-    console.log(`Fisrt Name: ${FIRST_NAME}`);
-    console.log(`Last Name: ${LAST_NAME}`);
     console.log(`About me: ${ABOUT}`);
     console.log(`Location: ${LOCATION}`);
     console.log(`Language: ${LANGUAGE}`);
-    console.log(`Experience work: ${WORK}`);
+    WORK_USER.forEach((checkbox) => {
+        const isChecked = checkbox.checked;
+        const value = checkbox.value;
+        if (isChecked) {
+            console.log(`Experience: ${value}`);
+        } else if (isChecked === checkbox.unchecked) {
+            console.log('Pomylka');
+        }
+    });
 })
