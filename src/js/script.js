0dc2acd699abb7ae8a649d577ba98c92bbe2b928
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-02-20 11:59:53 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -9,21 +9,26 @@ const WORK_USER = document.querySelectorAll(`input[type="checkbox"]`);;
 FORM.addEventListener("submit", (event) => {
     event.preventDefault();
     const FIRST_NAME = FNAME.value;
+    const LAST_NAME = LNAME.value;
+    const ABOUT = ABOUT_ME.value;
+    const LOCATION = LOCATION_DATA.value;
+    const LANGUAGE = LANGUAGE_USER.value;
+
     function capitalize(FNAME) {
         return `${FNAME.charAt(0).toUpperCase()}${FNAME.slice(1)}`;
     }
     console.log('First Name: ' + capitalize(`${FIRST_NAME}`))
-    
-    const LAST_NAME = LNAME.value;
     function capitalize(LNAME) {
         return `${LNAME.charAt(0).toUpperCase()}${LNAME.slice(1)}`;
     }
-    console.log('Last Name: ' + capitalize(`${LNAME}`))
-    const ABOUT = ABOUT_ME.value;
-    const LOCATION = LOCATION_DATA.value;
-    const LANGUAGE = LANGUAGE_USER.value;
+    console.log('Last Name: ' + capitalize(`${LAST_NAME}`))
     console.log(`About me: ${ABOUT}`);
-    console.log(`Location: ${LOCATION}`);
+
+    function capitalize(LOCATION_DATA) {
+        return `${LOCATION_DATA.charAt(0).toUpperCase()}${LOCATION_DATA.slice(1)}`;
+    }
+    console.log('Location: ' + capitalize(`${LOCATION}`))
+
     console.log(`Language: ${LANGUAGE}`);
     WORK_USER.forEach((checkbox) => {
         const isChecked = checkbox.checked;
