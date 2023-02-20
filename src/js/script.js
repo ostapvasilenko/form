186eb78194caa5c2726d01110b5254f292714e08
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-02-20 13:05:17 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,37 @@
-
 $('form').on('submit', function (event) {
     event.preventDefault();
     var form = $("form").serializeArray();
-    console.log(form)
+
+    for (var i = 0; i < form.length; i++) {
+        console.log(form[i]);
+    }
 });
+function showDataInModal(data) {
+    // Отримуємо посилання на модальне вікно
+    const modal = document.getElementById('modal');
+
+    // Створюємо елемент списку
+    const list = document.createElement('ul');
+
+    // Додаємо кожен об'єкт з масиву до списку
+    data.forEach(item => {
+        const listItem = document.createElement('li');
+        const text = document.createTextNode(JSON.stringify(item));
+        listItem.appendChild(text);
+        list.appendChild(listItem);
+    });
+
+    // Очищаємо вміст модального вікна та додаємо до нього список
+    modal.innerHTML = '';
+    modal.appendChild(list);
+
+    // Показуємо модальне вікно
+    modal.style.display = 'block';
+} 
+const data = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Mary' },
+    { id: 3, name: 'Bob' }
+];
+
+showDataInModal(data);
