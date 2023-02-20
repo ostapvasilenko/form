71454836e94f13b1927b6cee24eb62ca353e6938
ostapvasulenko <ostapvasilenko55@gmail.com>
2023-02-20 21:35:32 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -9,7 +9,7 @@
 
 
 function displayData() {
-    const FORM = document.getElementById("form").value;
+    const FORM = document.querySelector('#form');
     const FIRST_NAME = document.getElementById("fname").value;
     const LAST_NAME = document.getElementById("lname").value;
     const ABOUT_ME = document.getElementById("about__me").value;
@@ -25,3 +25,22 @@ function displayData() {
     RESULT.innerHTML = "Ім'я: " + FIRST_NAME + "<br>" + "Прізвище: " + LAST_NAME + "<br>" + "Про мене: " + ABOUT_ME + "<br>" + "Локація: " + LOCATION + "<br>" + "Мови спілкування: " + LANGUAGE + "<br>" + "Досвід роботи: " + EXPERIENCE  + "<br>" + "Термін, який готова працювати: " + TERM_OF_WORK;
 }
 
+const FIRST_NAME = document.querySelector('#fname');
+const LAST_NAME = document.querySelector('#lname');
+
+form.addEventListener('input', (event) => {
+  // отримати значення полів форми
+  const value1 = FIRST_NAME.value;
+  const value2 = LAST_NAME.value;
+  
+  // зберегти значення у localStorage
+  localStorage.setItem('myFormValues', JSON.stringify({ value1, value2 }));
+});
+
+// перевірити, чи містяться дані у localStorage
+const storedValues = JSON.parse(localStorage.getItem('myFormValues'));
+if (storedValues) {
+  // встановити значення полів форми відповідно до даних у localStorage
+  FIRST_NAME.value = storedValues.value1;
+  LAST_NAME.value = storedValues.value2;
+}
