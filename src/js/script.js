aad80f657818fa12643b4d7064e2316f668a0c93
ostapvasulenko <ostapvasilenko55@gmail.com>
2023-02-20 21:09:27 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,24 @@
-$('form').on('submit', function (event) {
-    event.preventDefault();
-    var form = $("form").serializeArray();
-    localStorage.setItem("form", JSON.stringify(form));
-    for (var i = 0; i < form.length; i++) {
-        console.log(form[i]);
-    }
-});
+// $('form').on('submit', function (event) {
+//     event.preventDefault();
+//     var form = $("form").serializeArray();
+//     localStorage.setItem("form", JSON.stringify(form));
+//     for (var i = 0; i < form.length; i++) {
+//         console.log(form[i]);
+//     }
+// });
 
 
 function displayData() {
-    const name = document.getElementById("fname").value;
-    const email = document.getElementById("lname").value;
-    const result = document.getElementById("result");
-  
-    result.innerHTML = "Ім'я: " + name + "<br>" + "Прізвище: " + email;
-  }
+    const FORM = document.getElementById("form").value;
+    const FIRST_NAME = document.getElementById("fname").value;
+    const LAST_NAME = document.getElementById("lname").value;
+    const ABOUT_ME = document.getElementById("about__me").value;
+    const LOCATION = document.getElementById("location").value;
+    const LANGUAGE = document.getElementById("lang").value;
+    const EXPERIENCE =document.getElementsByName("experience").value
+    const TERM_OF_WORK =document.getElementsByName("term of work").value
+    const RESULT = document.getElementById("result");
+    
+
+    RESULT.innerHTML = "Ім'я: " + FIRST_NAME + "<br>" + "Прізвище: " + LAST_NAME + "<br>" + "Про мене: " + ABOUT_ME + "<br>" + "Локація: " + LOCATION + "<br>" + "Мови спілкування: " + LANGUAGE + "<br>" + "Досвід роботи: " + EXPERIENCE  + "<br>" + "Термін, який готова працювати: " + TERM_OF_WORK;
+}
